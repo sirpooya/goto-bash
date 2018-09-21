534881b0e3a6d340feabeaa65ebe20f39e2f985f
pooyak <pooyak@live.com>
2018-09-21 20:22:41 +0430

    updated .link & help file

--- goto.sh
+++ goto.sh
@@ -17,12 +17,12 @@ uplink() {
     if [[ ! -f $link_file ]]; then
     echo "No Link file found"
     else 
-        item=`grep ":" ".link"`;
+        item=`grep "→" ".link"`;
         if [[ -z $item ]]; then
             echo 'No items found to Link'
         else
-            file=`echo "$item" | cut -d\: -f1`
-            link=`echo "$item" | cut -d\: -f2`
+            file=`echo "$item" | cut -d\→ -f1`
+            link=`echo "$item" | cut -d\→ -f2`
             ln -vif "$file" "$link"
             chmod +x "$link"
             #. ~/.bash_profile
@@ -45,6 +45,8 @@ opent() {
     osascript  -e 'tell application "Finder"' -e 'activate' -e 'tell application "System Events"' -e 'keystroke "t" using command down' -e 'end tell' -e 'set target of front Finder window to ("'$url'" as POSIX file)' -e 'end tell' -e 'say "'$parent'"' ; cecho "🙂 Opening \"$parent\" ...";
 }
 
+
+
 goto() {
     while [ $# -gt 0 ]; do
         arg=$1;
@@ -60,7 +62,7 @@ goto() {
                 cecho "$info";
                 break ;;
 
-            "share"     )
+            "share/-cp"     )
                 adr=$PWD
                 echo -n $adr | pbcopy
                 break;;
@@ -69,7 +71,7 @@ goto() {
                 showHelp;
                 break ;;
 
-            "-a" | * )
+            "-b" )
                 showHelp;
                 break ;;
         esac
@@ -92,8 +94,18 @@ showHelp () {
     echo;
     echo "    opent                             # Open current directory in new Finder Tab.";
     echo "    opent <location>                  # Open location in new Finder Tab.";
-    echo "    go <bookmark name>                # Gos to directory.";
-    echo "    save <bookmark name>              # make new alias to directory.";
+    echo;
+    echo "    go                                # Shows help.";
+    echo "    go <directory>                    # Goes to directory.";
+    echo "    go -all                           # Shows all bookmarks.";
+    echo "    go <bookmark name>                # Goes to bookmarked directory.";
+    echo "    go -b                             # Saves current directory to bookmarks with original folder name .";
+    echo "    go -b <bookmark name>             # Saves current directory to bookmarks with given name";   
+    echo;
+    echo "    mkals                             # Makes Finder Alias";
+    echo "    search                            # Searchs for a keyword in files & folders";
+    echo "    own                               # Change file ownership as root & executable.";
+    echo;
     echo "    help                              # show help file.";
     echo "    --version                         # Show version.";
     echo;
@@ -116,3 +128,57 @@ showHelp () {
 # T0D0 : version-ing be like:
 # $ goto --version
 # Goto v1.0.0
+
+
+
+"-b"    )
+    if [ $# != 1 ]; then
+        # There are additional arguments, so find out how many
+        array=( $@ );
+        len=${#array[@]};
+
+        searchAndPlay() {
+            type="$1"
+            Q="$2"
+        }
+
+        case $2 in
+            "list"  )
+                _args=${array[@]:2:$len};
+                Q=$_args;
+
+                cecho "Searching playlists for: $Q";
+
+                results=$( \
+                    curl -s -G $SPOTIFY_SEARCH_API --data-urlencode "q=$Q" -d "type=playlist&limit=10&offset=0" -H "Accept: application/json" -H "Authorization: Bearer ${SPOTIFY_ACCESS_TOKEN}" \
+                    | grep -E -o "spotify:user:[a-zA-Z0-9_]+:playlist:[a-zA-Z0-9]+" -m 10 \
+                )
+
+                count=$( \
+                    echo "$results" | grep -c "spotify:user" \
+                )
+
+            "album" | "artist" | "track"    )
+                _args=${array[@]:2:$len};
+                searchAndPlay $2 "$_args";;
+
+            "uri"  )
+                SPOTIFY_PLAY_URI=${array[@]:2:$len};;
+
+            *   )
+                _args=${array[@]:1:$len};
+                searchAndPlay track "$_args";;
+        esac
+
+        if [ "$SPOTIFY_PLAY_URI" != "" ]; then
+            cecho "Playing Spotify URI";
+        else
+            cecho "No results when searching";
+        fi
+
+    else
+        # Show Bookmarks
+        cecho "Playing Spotify.";
+        osascript -e 'tell application "Spotify" to play';
+    fi
+    break ;;
