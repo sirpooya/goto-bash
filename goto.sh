7117168b9a1cba41324761a9c9987328d1b3dfb8
pooyak <pooyak@live.com>
2018-09-21 16:15:40 +0430

    cleaned file

--- goto.sh
+++ goto.sh
@@ -39,9 +39,19 @@ goto() {
     elif [ $# = "ver" ] ; then 
     echo "Goto 1.0"; 
     else url="$1"; 
-    fi ; 
+    fi ;
     osascript -e 'tell application "Finder"' -e 'activate' -e 'tell application "System Events"' -e 'keystroke "t" using command down' -e 'end tell' -e 'set target of front Finder window to ("'$url'" as POSIX file)' -e 'end tell' -e 'say ""' ;
- }
+}
+
+
+
+
+
+
+
+
+
+
 
 showVersion () {
     echo "goto v1.0.0";
@@ -58,93 +68,12 @@ showHelp () {
     echo "  save <bookmark name>             # make new alias to directory.";
 }
 
-if [ $# = 0 ]; then
-    showHelp;
-fi
-
-while [ $# -gt 0 ]; do
-    arg=$1;
+#if [ $# = 0 ]; then
+    #showHelp;
+#fi
 
 
-while [ $# -gt 0 ]; do
-    arg=$1;
-
-    case $arg in
-        "play"    )
-            if [ $# != 1 ]; then
-                # There are additional arguments, so find out how many
-                array=( $@ );
-                len=${#array[@]};
-
-                searchAndPlay() {
-                    type="$1"
-                    Q="$2"
-                }
-
-                case $2 in
-                    "list"  )
-                        _args=${array[@]:2:$len};
-                        Q=$_args;
-
-                        cecho "Searching playlists for: $Q";
-
-                        results=$( \
-                            curl -s -G $SPOTIFY_SEARCH_API --data-urlencode "q=$Q" -d "type=playlist&limit=10&offset=0" -H "Accept: application/json" -H "Authorization: Bearer ${SPOTIFY_ACCESS_TOKEN}" \
-                            | grep -E -o "spotify:user:[a-zA-Z0-9_]+:playlist:[a-zA-Z0-9]+" -m 10 \
-                        )
-
-                        count=$( \
-                            echo "$results" | grep -c "spotify:user" \
-                        )
-
-                    "album" | "artist" | "track"    )
-                        _args=${array[@]:2:$len};
-                        searchAndPlay $2 "$_args";;
-
-                    "uri"  )
-                        SPOTIFY_PLAY_URI=${array[@]:2:$len};;
-
-                    *   )
-                        _args=${array[@]:1:$len};
-                        searchAndPlay track "$_args";;
-                esac
-
-                if [ "$SPOTIFY_PLAY_URI" != "" ]; then
-                    cecho "Playing Spotify URI";
-                else
-                    cecho "No results when searching";
-                fi
-
-            else
-
-                # play is the only param
-                cecho "Playing Spotify.";
-                osascript -e 'tell application "Spotify" to play';
-            fi
-            break ;;
-
-        "--version" )
-            showVersion;
-            break ;;
-
-        "info" )
-            info=`osascript -e 'tell application "Finder"
-            end tell
-            return 0'`
-            cecho "$info";
-            break ;;
-
-        "share"     )
-            uri=$PWD
-            echo -n $uri | pbcopy
-            break;;
-
-        "help" | * )
-            showHelp;
-            break ;;
-    esac
-done
-# add shorcuts to this function
+# T0DO : add shorcuts to this function
 # $ goto -a apps /Applications
 # $ goto -a
 # apps     docs    sites
@@ -152,6 +81,6 @@ done
 # $ goto photos
 # Shortcut not found
 
-# version-ing be like:
+# T0D0 : version-ing be like:
 # $ goto --version
 # Goto v1.0.0
